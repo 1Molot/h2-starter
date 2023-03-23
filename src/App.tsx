22263e43f3b4895df233f2e428b3d702887962ac
1Molot <sinkevich1990@gmail.com>
2023-03-23 19:12:54 +0300

    fix: h2-starter

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {SuperButton} from "./components/SuperButton";
+import {Adidas} from "./components/Adidas";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const prices = [
+        { id: 1, model: "HTML&CSS", price: 100200 },
+        { id: 2, model: "HTML&CSS222", price: 100200 },
+        { id: 3, model: "HTML&CSS333", price: 100200 },
+        { id: 4, model: "HTML&CSS444", price: 100200 },
+
+    ]
+
+
+    return (
+        <div>
+            {/*<SuperButton name={'RedButton'} callBack={()=>{}} color={'red'}/>*/}
+
+
+            <Adidas prices={prices}>
+                <SuperButton  callBack={()=>{}} color={'red'}>RedButton</SuperButton>
+                <SuperButton  callBack={()=>{}} color={'secondary'}>RedButton</SuperButton>
+                <SuperButton  callBack={()=>{}} disabled>RedButton</SuperButton>
+            </Adidas>
+            <Adidas prices={prices}>
+                <div>
+                    <input type="text"/>
+                </div>
+                <div>
+                    <input type="text"/>
+                </div>
+                <div>
+                    <input type="text"/>
+                </div>
+
+                <input type="text"/>
+                <input type="text"/>
+                <SuperButton  callBack={()=>{}} disabled>RedButton</SuperButton>
+            </Adidas>
+            <Adidas prices={prices}></Adidas>
+
+
+        </div>
+    );
 }
+
+
 
 export default App;
--- src/components/Adidas.tsx
+++ src/components/Adidas.tsx
@@ -1,3 +1,30 @@
 import React from 'react';
 
-export
+type dataType = {
+    id: number,
+    model: string,
+    price: number
+}
+type PropsType = {
+    prices: dataType[]
+    children?: React.ReactNode
+}
+
+
+export const Adidas = (props: PropsType) => {
+    return (
+        <div>
+            {props.prices.map(el => {
+                return (
+                    <div>
+                        <span>{el.id}</span>
+                        <span>{el.model}</span>
+                        <span>{el.price}</span>
+                    </div>
+                )
+            })}
+            <div>{props.children}</div>
+            <div>-----------------------------------------------------------------</div>
+        </div>
+    );
+};
